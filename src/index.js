3ca428a2073b45bedb23c9c19f755ae604ebe785
Levi Miller <paullevimiller@gmail.com>
2019-09-10 13:23:11 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  function addTask() {
+    const listNode = document.createElement("li");
+    const submittedTaskDescription = newTaskDescription.value;
+    console.log(submittedTaskDescription);
+    const textNode = document.createTextNode(submittedTaskDescription);
+    listNode.appendChild(textNode);
+    taskList.appendChild(listNode);
+  }
+
+  createTaskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addTask();
+  });
 });
+
